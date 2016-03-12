a8fcc4b7028b734bf923071ed8f80469854048a2
Gabe Lerner <gabelerner@gmail.com>
2016-03-12 12:34:54 -0600

    form flow

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,14 @@ class WelcomeController < ApplicationController
   def submit_email
     @email = params[:email]
   end
+  
+  def submit_info
+    # params[:sam_text_input]
+    # params[:sam_text_area]
+    # params[:sam_select]
+    # params[:sam_checkbox_1]
+    # params[:sam_checkbox_2]
+    # params[:sam_checkbox_3]
+    @params = params
+  end
 end
